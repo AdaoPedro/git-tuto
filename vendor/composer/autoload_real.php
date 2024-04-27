feb27b1cc8d5227deb8d8edee8ed8d49e9deb3af
apedrodevelopers <contacto@apedrodevelopers.ao>
2024-04-27 06:19:33 +0100

    Add var-dumper

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit1487a8e0a6d435b41a105bc546ca6120
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit1487a8e0a6d435b41a105bc546ca6120', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit1487a8e0a6d435b41a105bc546ca6120', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit1487a8e0a6d435b41a105bc546ca6120
         call_user_func(\Composer\Autoload\ComposerStaticInit1487a8e0a6d435b41a105bc546ca6120::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit1487a8e0a6d435b41a105bc546ca6120::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
